ec2f18ac5ea2473ac67f015e9a055828ada66f2e
kekekuli <kekekuliobj@gmail.com>
2025-06-18 17:47:22 +0800

    root, contact, edit routes

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,9 +1,40 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactsId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+    loader: rootLoader,
+    action: rootAction,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>hello</div>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
